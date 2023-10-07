3f07303e1a4eff290eee82048cacee4cd05c7314
pelinaybar <aybarpelin00@gmail.com>
2023-10-07 19:54:37 +0300

    ChatListViewPart1

---
+++ ChatGPTSwiftUI/ChatList/ChatListView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatListView.swift
+//  ChatGPTSwiftUI
+//
+//  Created by Pelin AY on 7.10.2023.
+//
+
+import SwiftUI
+
+struct ChatListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatListView()
+}
---
+++ ChatGPTSwiftUI/ChatList/ChatListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChatListViewModel.swift
+//  ChatGPTSwiftUI
+//
+//  Created by Pelin AY on 7.10.2023.
+//
+
+import Foundation
